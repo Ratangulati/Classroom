faf3d67adce68cf221c0e0cbdbd9d847601d70c0
Ratan Gulati <ratangulati.dev@gmail.com>
2024-06-30 19:34:51 +0530

    updated code
    Signed-off-by: Ratan Gulati <ratangulati.dev@gmail.com>

--- backend/api/index.js
+++ backend/api/index.js
@@ -25,11 +25,16 @@ app.use((err, req, res, next) => {
 })
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL, 
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
     optionsSuccessStatus: 200
 };
 
+
 app.use(cors(corsOptions)); 
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
